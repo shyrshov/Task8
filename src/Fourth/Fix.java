ced7874bc52ac121286b26045e475a3d537bd6b3
Andrii <a.shyrshov@gmail.com>
2021-04-07 22:37:04 +0300

    Fourth

---
+++ src/Fourth/Fix.java
@@ -0,0 +1,15 @@
+package Fourth;
+
+public class Fix {
+
+    public static void main(String[] args) {
+        try {
+            int a = 0;
+            int b = 4 / a;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
